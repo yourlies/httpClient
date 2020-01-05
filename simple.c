fe7ddd539bde609bac69da9dff14a3fe3eb90993
jinxin <958142428@qq.com>
2020-01-05 14:47:56 +0800

    auto push

--- simple.c
+++ simple.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
+#ifdef __WIN32__
+#include <ws2tcpip.h>
+#include <winsock2.h>
+#else
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
+#endif
 #include <sys/types.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <sys/time.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+
+#include "lib/socket.h"
 
 #define IPSTR "127.0.0.1"
 #define PORT 8080
 #define BUFSIZE 1024
+SOCKET sockfd;
+WSADATA wsd;
 
 int main(int argc, char **argv)
 {
-  int sockfd, ret, i, h;
+  int ret, i, h;
   struct sockaddr_in servaddr;
   char str1[4096], str2[4096], buf[BUFSIZE], *str;
   socklen_t len;
@@ -25,26 +33,33 @@ int main(int argc, char **argv)
   struct timeval tv;
 
   //创建套接字
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
   {
-    printf("创建网络连接失败,本线程即将终止---socket error!\n");
-    exit(0);
-  };
+    printf("WSA start up failed!\n");
+    return 1;
+  }
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sockfd == INVALID_SOCKET)
+  {
+    printf("Socket invalid!\n");
+    return 2;
+  }
 
-  bzero(&servaddr, sizeof(servaddr));
+  memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_port = htons(PORT);
-  if (inet_pton(AF_INET, IPSTR, &servaddr.sin_addr) <= 0)
+  servaddr.sin_addr.s_addr = inet_addr(IPSTR);
+  if (servaddr.sin_addr.s_addr <= 0)
   {
     printf("创建网络连接失败,本线程即将终止--inet_pton error!\n");
     exit(0);
   };
 
-  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == SOCKET_ERROR)
   {
-    printf("连接到服务器失败,connect error!\n");
-    exit(0);
+    printf("%s", "test");
   }
+
   printf("与远端建立了连接\n");
   memset(str2, 0, 4096);
   strcat(str2, "?username=tom&password=111111");
@@ -62,23 +77,13 @@ int main(int argc, char **argv)
 
   strcat(str1, "\r\n\r\n");
   printf("%s\n", str1);
-
-  ret = write(sockfd, str1, strlen(str1));
-  if (ret < 0)
-  {
-    printf("发送失败！错误代码是%d，错误信息是'%s'\n", errno, strerror(errno));
-    exit(0);
-  }
-  else
-  {
-    printf("消息发送成功，共发送了%d个字节！\n\n", ret);
-  }
+  ret = send(sockfd, str1, strlen(str1), 0);
 
   FD_ZERO(&t_set1);
   FD_SET(sockfd, &t_set1);
 
   memset(buf, 0, sizeof(buf));
-  read(sockfd, buf, 4095);
+  recv(sockfd, buf, 4095, 0);
   printf("%s\n", buf);
   close(sockfd);
 
